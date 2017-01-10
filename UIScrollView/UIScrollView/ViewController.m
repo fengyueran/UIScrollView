12255d526d9fa2bf6b3ef56d09d8dfbbe22f387c
snow <snow@snowdeMBP.lan>
2017-01-10 23:11:49 +0800

    add scroll view

--- UIScrollView/UIScrollView/ViewController.m
+++ UIScrollView/UIScrollView/ViewController.m
@@ -9,6 +9,12 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIButton *bottom;
+- (IBAction)right:(id)sender;
+- (IBAction)top:(id)sender;
+- (IBAction)left:(id)sender;
+- (IBAction)bottom:(id)sender;
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
 
 @end
 
@@ -16,12 +22,56 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+//    UIImageView *imageView = [[UIImageView alloc]init];
+//    imageView.image = [UIImage imageNamed:@"minion"];
+//    imageView.frame = CGRectMake(0, 0, imageView.image.size.width, imageView.image.size.height);
+    //imageView大小等于image
+    UIImageView *imageView = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"minion"]];
+    [self.scrollView addSubview:imageView];
+    //设置滚动范围
+    self.scrollView.contentSize = imageView.image.size;
+    
+    //设置额外滚动范围
+    self.scrollView.contentInset = UIEdgeInsetsMake(80, 40, 30, 10);
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)left:(id)sender {
+   // self.scrollView.contentOffset:偏移量
+    //内容的左上角和scrollView左上角的x/y差值
+//    self.scrollView.contentOffset = CGPointMake(100, 100);//左上
+    [UIView beginAnimations:nil context:nil];
+    [UIView setAnimationDuration:1];
+    [UIView setAnimationDelegate:self];
+    [UIView setAnimationDidStopSelector:@selector(stop)];
+    [UIView setAnimationWillStartSelector:@selector(start)];
+    self.scrollView.contentOffset = CGPointMake(0, self.scrollView.contentOffset.y);
+    [UIView commitAnimations];
 }
 
+- (void)start {
+    NSLog(@"animation start");
+}
+
+- (void)stop {
+    NSLog(@"animation stop");
+}
+
+- (IBAction)right:(id)sender {
+    [UIView animateWithDuration:1 animations:^{
+    self.scrollView.contentOffset = CGPointMake(self.scrollView.contentSize.width - self.scrollView.frame.size.width, self.scrollView.contentOffset.y);
+    }];
+
+}
+
+- (IBAction)top:(id)sender {
+    self.scrollView.contentOffset = CGPointMake(self.scrollView.contentOffset.x, 0);
+    //不允许直接修改OC对象的结构体成员属性
+//    self.scrollView.contentOffset.y = 10;
+}
+
+
+
+- (IBAction)bottom:(id)sender {
+    self.scrollView.contentOffset = CGPointMake(self.scrollView.contentOffset.x, self.scrollView.contentSize.height - self.scrollView.frame.size.height);
+}
 @end
