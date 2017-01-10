af8e74575c803588e3ed34298090935b5a6234ab
snow <snow@snowdeMBP.lan>
2017-01-10 21:46:36 +0800

    init the repository

---
+++ UIScrollView/UIScrollView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UIScrollView
+//
+//  Created by snow on 17/1/10.
+//  Copyright © 2017年 snow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
